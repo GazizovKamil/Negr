5717a56263a4c4deece260cf3e7f5aff4c23e590
Radel Zubairov <gggg26705@gmail.com>
2022-10-04 15:07:00 +0300

    Добавьте файлы проекта.

---
+++ Negr/ClassApp/DBConection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Negr;
+using System.Threading.Tasks;
+
+namespace Negr.ClassApp
+{
+    internal class DBConection
+    {
+        public static GreenAntiCafeWithBearEntities GreenAntiCafeWithBearEntities = new GreenAntiCafeWithBearEntities();
+    }
+}
